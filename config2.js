1dead29d348c9f784585f8bbcb45fc1ba7f847d0
Dataanalyst-11 <mis.philmed01.dpt@gmail.com>
2026-08-10 15:06:46 +0300

    Add files via upload

---
+++ config2.js
@@ -0,0 +1,11 @@
+// config.js
+// Central configuration for all dashboards
+
+const API_CONFIG = {
+  API_URL: "https://api.beibora.co.ke/invoice-tracker/order-tracking/",
+  API_TOKEN: "jZYyPDJG1oZjAHhgFJPfy2fl8"
+};
+
+// Expose it globally for all dashboards
+window.API_CONFIG = API_CONFIG;
+
