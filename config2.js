7ec37f77fb6fb9eb5cdebca7aea16aa92c0229b4
Dataanalyst-11 <mis.philmed01.dpt@gmail.com>
2026-09-02 11:23:19 +0300

    Update config2.js

--- config2.js
+++ config2.js
@@ -2,7 +2,7 @@
 // Central configuration for all dashboards
 
 const API_CONFIG = {
-  API_URL: "https://api.beibora.co.ke/invoice-tracker/order-tracking/",
+  API_URL: "https://api.beibora.co.ke/invoice-tracker/",
   API_TOKEN: "jZYyPDJG1oZjAHhgFJPfy2fl8"
 };
 
